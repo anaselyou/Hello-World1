b3835f886f83ccfc92fe9642b82b80d601c89822
Anas Elyou <anas.elyou.mail@gmail.com>
2025-10-20 19:23:44 +0200

    Test 1

--- src/my/compagny/com/Hello1/Helloworld.java
+++ src/my/compagny/com/Hello1/Helloworld.java
@@ -1,8 +1,17 @@
 package my.compagny.com.Hello1;
+import java.util.Scanner;
 
 public class Helloworld {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
         System.out.println("Hello World");
+        String number1 = "Je vous souhaite tout le bonheur du monde ! ";
+
+        System.out.println(number1);
+
+
     }
 
 }
